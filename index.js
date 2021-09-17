6bb8338bf74e9654f38eff8283913a7e97c89bf4
Zane Hitchcox <zane@balena.io>
2021-09-17 15:51:20 -0400

    publish to npm
    Change-type: major

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const {
     newStreamDuplexFromReadableWritablePair,
+    newReadableWritablePairFromDuplex,
     newReadableStreamFromStreamReadable,
     newWritableStreamFromStreamWritable,
     newStreamWritableFromWritableStream,
-    newReadableStreamFromStreamReadable,
+    newStreamReadableFromReadableStream,
 } = require('./lib/conversions');
 
 const {
@@ -17,10 +18,11 @@ const {
 
 module.exports = {
     newStreamDuplexFromReadableWritablePair,
+    newReadableWritablePairFromDuplex,
     newReadableStreamFromStreamReadable,
     newWritableStreamFromStreamWritable,
     newStreamWritableFromWritableStream,
-    newReadableStreamFromStreamReadable,
+    newStreamReadableFromReadableStream,
     toNode,
     toWeb,
     pipeline,
